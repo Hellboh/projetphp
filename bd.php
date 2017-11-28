e86ab1474214d99639dc676e217349d7321549f9
Your Name <you@example.com>
2017-11-28 21:41:06 +0100

    connect to database

---
+++ bd.php
@@ -0,0 +1,14 @@
+<?php 
+
+  try {
+
+    $db = new PDO( 'mysql:host=localhost;dbname=base', 'root', '' );
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+  } catch(PDOException $e) {
+
+    die( $e->getMessage() );
+
+  }
+
+?>
